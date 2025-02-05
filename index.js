bd9ce5aafb593dd673862ed4a2d4d5d0949f950d
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-02-05 20:48:56 +0530

    graphql new

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express")
-const {graphqlHTTP} = require("express-graphql")
-
+const {graphqlHTTP} = require("express-graphql");
+const { GraphQLObjectType, GraphQLID, GraphQLString } = require("graphql");
 const app = express();
+
+const userType = new GraphQLObjectType({
+    name: "userType",
+    fields: ()=>({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString}
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQuery",
+    fields: {}
+})
 
 app.use("/graphql", graphqlHTTP({schema:{}, graphiql: true}))
 
