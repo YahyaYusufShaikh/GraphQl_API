5af940e96ab13966096fb2d9ff6b088f58c4b068
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-02-05 20:42:03 +0530

    graphql_

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const {graphqlHTTP} = require("express-graphql")
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({schema:{}, graphiql: true}))
+app.use("/graphql", graphqlHTTP({schema:{}, graphiql: false}))
 
 app.listen(5000, ()=>console.log("server started"))
